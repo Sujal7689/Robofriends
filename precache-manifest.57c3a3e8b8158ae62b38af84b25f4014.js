a53efc9a543eea70fe6ab2a7d5df6d4f9893b9e4
Sujal7689 <63237646+Sujal7689@users.noreply.github.com>
2020-05-03 16:24:38 +0545

    Add files via upload

---
+++ precache-manifest.57c3a3e8b8158ae62b38af84b25f4014.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3625479ef25b6b521cb026acb254317",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "a592d1bf809a6c87ccbe",
+    "url": "/static/css/main.788bff12.chunk.css"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/static/js/2.479e6b44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.479e6b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a592d1bf809a6c87ccbe",
+    "url": "/static/js/main.38995e8e.chunk.js"
+  },
+  {
+    "revision": "46b2ab5944883490d30d",
+    "url": "/static/js/runtime-main.501fdd23.js"
+  }
+]);
